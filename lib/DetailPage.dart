e3c19b67ad392130b815058d31f8a3d85b986988
DESKTOP-NFNJ853\Minnie <minnie@binodata.com>
2020-04-06 23:10:47 +0800

    新增登入頁面

---
+++ lib/DetailPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'helpers/Constants.dart';
+
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
---
+++ lib/helpers/Constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// Colors
+Color appPinkGrayColor = Color.fromRGBO(227, 191, 191, 1.0);
+Color appGrayPinkColor = Color.fromRGBO(130, 116, 116, 1.0);
+Color appPinkColor = Color.fromRGBO(222, 160, 160, 1.0);
+
+// Strings
+const appTitle = "MyCalendar";
+const accountText = "Account";
+const passwordText = "Password";
+const accountErrorText = "帳號格式錯誤";
+const passwordErrorText = "密碼格式錯誤";
+const loginButtonText = "Login";
+
+// Images
+Image appLogo = Image.asset('assets/images/calendar.png');
+
+// Sizes
+const bigRadius = 66.0;
+const buttonHeight = 24.0;
+const spaceHeight = 12.0;
